88149ac9be932009d03d7ac700b8da92b9cc7c80
Dostonbek Matyakubov <57319534+doston12@users.noreply.github.com>
2024-07-16 21:42:09 +0100

    Update conftest.py to parse --language

--- conftest.py
+++ conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture()
-def browser():
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default=None, help="Choose browser language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
     print("\nstart browser for test...")
-    browser = webdriver.Chrome()
+    language = request.config.getoption("language")
+    chrome_options = Options()
+
+    chrome_options.add_experimental_option("prefs", {"intl.accept_languages": str(language)})
+    browser = webdriver.Chrome(options=chrome_options)
+
     yield browser
     print("\nquit browser...")
     browser.quit()
-
