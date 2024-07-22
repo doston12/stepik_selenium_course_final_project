56f4c4bf742cd8c44804915d7a68c406967b5016
Dostonbek Matyakubov <57319534+doston12@users.noreply.github.com>
2024-07-22 20:58:58 +0100

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,13 +2,10 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage):
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.ID, "registration_link")
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link isn't present"
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
 
 
