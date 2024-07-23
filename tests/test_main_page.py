6a5675302b3d4a93f9f700b896d93648466a7d04
Dostonbek Matyakubov <57319534+doston12@users.noreply.github.com>
2024-07-23 20:59:07 +0100

    Update test_main_page.py

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,5 @@
+from pages.basket_page import BasketPage
+from pages.locators import BasketPageLocators
 from pages.main_page import MainPage
 
 
@@ -8,3 +10,16 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     page.should_be_login_link()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    main_page = MainPage(browser, link)
+    main_page.open()
+
+    main_page.open_basket()
+
+    basket_page = BasketPage(browser, link)
+    assert basket_page.is_not_element_present(BasketPageLocators.ITEM_NAMES), "There are no elements in the basket"
+
+    assert "Your basket is empty." in basket_page.browser.find_element(*BasketPageLocators.BASKET_EMPTY).text, \
+        "Basket empty text is shown"
